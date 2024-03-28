49589d8774ac3a75da157288e5dd318df5b5b943
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-03-28 01:48:42 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -5,6 +5,5 @@
 <a href="https://dosen.stih-painan.ac.id/" rel="dofollow">Slot Gacor</a>
 <a href="http://103.47.60.38/icons/sthailand/" rel="dofollow">Slot Gacor</a>
 <a href="http://103.47.60.38/sgacor/" rel="dofollow">Slot Gacor</a>
-<a href="http://bapenda.malukutenggarakab.go.id/slotgacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://korem031.tniad.mil.id/wp-includes/pomo/sr-gacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://elapor.merauke.go.id/login/home/" rel="dofollow">Slot Gacor</a>
