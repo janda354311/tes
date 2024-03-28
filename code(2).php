93ec5267cf8f379f48869dd0081fb57faf7e075e
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-03-27 23:31:43 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -1,17 +1,10 @@
 <div style="display:none;">
 
-<a href="https://kknreguler.unsam.ac.id/madu303/" rel="dofollow">Slot Gacor</a>
-<a href="https://kknreguler.unsam.ac.id/mauslot/" rel="dofollow">Slot Gacor</a>
+<a href="https://kknreguler.unsam.ac.id/amp/s/slot-gacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/-/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
-<a href="https://divif2.kostrad.mil.id/-/slotgacor/" rel="dofollow">Slot Gacor</a>
-<a href="http://sipencatar.poltekpelsumbar.ac.id/-/maxwin/" rel="dofollow">Slot Gacor</a>
-<a href="http://alumni.poltekpelsumbar.ac.id/excel/" rel="dofollow">Slot Gacor</a>
-<a href="http://simkeu.poltekpelsumbar.ac.id/eplanning/promo/" rel="dofollow">Slot Gacor</a> 
-<a href="https://stih-painan.ac.id/-/slotgacor/" rel="dofollow">Slot Gacor</a> 
 <a href="https://dosen.stih-painan.ac.id/" rel="dofollow">Slot Gacor</a>
 <a href="http://103.47.60.38/icons/sthailand/" rel="dofollow">Slot Gacor</a>
 <a href="http://103.47.60.38/sgacor/" rel="dofollow">Slot Gacor</a>
 <a href="http://bapenda.malukutenggarakab.go.id/slotgacor/" rel="dofollow">Slot Gacor</a>
-<a href="https://korem031.tniad.mil.id/wp-content/core/" rel="dofollow">Slot Gacor</a>
-<a href="https://pussimpur.kodiklat-tniad.mil.id/wp-content/slotmaxwin/" rel="dofollow">Slot Gacor</a>
+<a href="https://korem031.tniad.mil.id/wp-includes/pomo/sr-gacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://elapor.merauke.go.id/login/home/" rel="dofollow">Slot Gacor</a>
