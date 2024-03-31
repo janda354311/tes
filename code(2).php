dcd5a31027bafd244d143560292d4c795d7ae75e
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-03-31 18:06:01 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -1,13 +1,14 @@
 <div style="display:none;">
 
-<a href="https://kknreguler.unsam.ac.id/amp/s/slot-gacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://kknreguler.unsam.ac.id/tinymce/" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/-/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://divif2.kostrad.mil.id/store/server-thailand/" rel="dofollow">Slot Gacor</a>
+<a href="https://ptsp.halal.go.id/store/server-thailand/" rel="dofollow">Slot Gacor</a>
+<a href="https://ptsp.halal.go.id/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://ptsp.halal.go.id/pelatihan/" rel="dofollow">Slot Gacor</a>
+<a href="https://stih-painan.ac.id/store/slot-thailand/" rel="dofollow">Slot Gacor</a>
 <a href="https://dosen.stih-painan.ac.id/" rel="dofollow">Slot Gacor</a>
 <a href="http://103.47.60.38/icons/sthailand/" rel="dofollow">Slot Gacor</a>
 <a href="http://103.47.60.38/sgacor/" rel="dofollow">Slot Gacor</a>
-<a href="https://korem031.tniad.mil.id/sr-gacor/" rel="dofollow">Slot Gacor</a>
-<a href="https://ptsp.halal.go.id/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
-<a href="https://ptsp.halal.go.id/pelatihan/" rel="dofollow">Slot Gacor</a>
 <a href="https://sp.sidomuncul.co.id/sp_mon/s-gacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://elapor.merauke.go.id/login/home/" rel="dofollow">Slot Gacor</a>
