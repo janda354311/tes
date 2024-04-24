d23c87f18b4936acc4e04a330284ae8d8b5a33c4
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-04-24 15:32:15 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -1,15 +1,13 @@
 <div style="display:none;">
 
-<a href="https://divif2.kostrad.mil.id/-/zgacor/" rel="dofollow">SLOT GACOR</a>
+<a href="https://divif2.kostrad.mil.id/-/sgacor/" rel="dofollow">SLOT GACOR</a>
+<a href="https://divif2.kostrad.mil.id/-/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://ptsp.halal.go.id/app/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://stih-painan.ac.id/store/slot-thailand/" rel="dofollow">SLOT GACOR</a>
 <a href="https://dashboard.global.unair.ac.id/repo/index.html" rel="dofollow">SLOT GACOR</a>
 <a href="https://sijaki-dev.jombangkab.go.id/-/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://miku.polines.ac.id/public/-/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://pgsd.unsam.ac.id/" rel="dofollow">SLOT GACOR</a>
-<a href="https://lpm.stkipmodernngawi.ac.id/situs-thailand/" rel="dofollow">SLOT GACOR</a>
-<a href="https://sumbar.demokrat.or.id/link-thailand/" rel="dofollow">SLOT GACOR</a>
-<a href="https://demokrat.or.id/-/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://upertis.ac.id/apps/sgacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://halmaheraselatankab.go.id/-/sgacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://siperbangkom.bpsdm.sultengprov.go.id/" rel="dofollow">Slot Gacor</a>
@@ -32,7 +30,7 @@
 <a href="https://www.diskominfo.halmaheraselatankab.go.id/wp-content/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
 <a href="https://dpmptsp.kendalkab.go.id/app/sgacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://stih-painan.ac.id/store/pg-slot/" rel="dofollow">SLOT DEMO</a>
-<a href="https://anjabpk.kemnaker.go.id/app/pdemo/" rel="dofollow">SLOT DEMO</a>
+<a href="https://anjabpk.kemnaker.go.id/v3/uploads/demo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://divif2.kostrad.mil.id/app/pdemo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://ptsp.halal.go.id/app/pgdemo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://dashboard.global.unair.ac.id/repo/pdemo.html" rel="dofollow">SLOT DEMO</a>
