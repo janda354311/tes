a33fc09c4ab85bc29430dcbfa82edcb5d2ebfead
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-04-25 04:53:37 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -2,12 +2,12 @@
 
 <a href="https://divif2.kostrad.mil.id/-/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://divif2.kostrad.mil.id/-/sgacor/" rel="dofollow">SLOT GACOR</a>
+<a href="https://kknreguler.unsam.ac.id/gacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://ptsp.halal.go.id/app/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://stih-painan.ac.id/store/slot-thailand/" rel="dofollow">SLOT GACOR</a>
 <a href="https://dashboard.global.unair.ac.id/repo/index.html" rel="dofollow">SLOT GACOR</a>
 <a href="https://sijaki-dev.jombangkab.go.id/-/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://miku.polines.ac.id/public/-/sgacor/" rel="dofollow">SLOT GACOR</a>
-<a href="https://pgsd.unsam.ac.id/" rel="dofollow">SLOT GACOR</a>
 <a href="https://upertis.ac.id/apps/sgacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://halmaheraselatankab.go.id/-/sgacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://siperbangkom.bpsdm.sultengprov.go.id/" rel="dofollow">Slot Gacor</a>
@@ -33,5 +33,7 @@
 <a href="https://anjabpk.kemnaker.go.id/v3/uploads/demo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://divif2.kostrad.mil.id/app/pdemo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://ptsp.halal.go.id/app/pgdemo/" rel="dofollow">SLOT DEMO</a>
+<a href="https://kknreguler.unsam.ac.id/demo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://dashboard.global.unair.ac.id/repo/pdemo.html" rel="dofollow">SLOT DEMO</a>
+
  
