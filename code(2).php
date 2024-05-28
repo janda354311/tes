1900e7e88aec65d0ad9a22b561ae99a10d20eaba
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-05-28 13:06:34 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -9,13 +9,16 @@
 <a href="https://miku.polines.ac.id/public/-/sgacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://akademika.itbi.ac.id/files/" rel="dofollow">SLOT GACOR</a>
 <a href="https://akuntansi.feb.upb.ac.id/depo/" rel="dofollow">Slot Gacor</a>
+<a href="https://sisfo.feb.upb.ac.id/js/" rel="dofollow">Slot Gacor</a>
+<a href="https://sisfo.feb.upb.ac.id/link-slotx-gacorz/" rel="dofollow">Slot Gacor</a>
+<a href="https://manajemen.feb.upb.ac.id/demo/" rel="dofollow">Slot Demo</a>
 <a href="https://simba.polines.ac.id/pendaftar/thailand/" rel="dofollow">Slot Gacor</a>
 <a href="https://daful.polines.ac.id/thailand/" rel="dofollow">Slot Gacor</a>
 <a href="https://sister.itbi.ac.id/css/" rel="dofollow">Slot Gacor</a>
 <a href="https://ujm.itbi.ac.id/pages/" rel="dofollow">Slot Gacor</a>
- <a href="https://alumni.uniflor.ac.id/raja168/" rel="dofollow">Slot Gacor</a>
- <a href="https://wbs.pekanbaru.go.id/storage/link-slotx-zgacor/" rel="dofollow">Slot Gacor</a>
- <a href="https://stih-painan.ac.id/store/pg-slot/" rel="dofollow">SLOT DEMO</a>
+<a href="https://alumni.uniflor.ac.id/raja168/" rel="dofollow">Slot Gacor</a>
+<a href="https://wbs.pekanbaru.go.id/storage/link-slotx-zgacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://stih-painan.ac.id/store/pg-slot/" rel="dofollow">SLOT DEMO</a>
 <a href="https://anjabpk.kemnaker.go.id/v3/uploads/demo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://divif2.kostrad.mil.id/app/pdemo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://ptsp.halal.go.id/app/pgdemo/" rel="dofollow">SLOT DEMO</a>
