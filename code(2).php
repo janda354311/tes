a5edee0331398a98c4c14fe739b1245d50dacb65
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-05-27 22:30:24 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -14,7 +14,8 @@
 <a href="https://sister.itbi.ac.id/css/" rel="dofollow">Slot Gacor</a>
 <a href="https://ujm.itbi.ac.id/pages/" rel="dofollow">Slot Gacor</a>
  <a href="https://alumni.uniflor.ac.id/raja168/" rel="dofollow">Slot Gacor</a>
-<a href="https://stih-painan.ac.id/store/pg-slot/" rel="dofollow">SLOT DEMO</a>
+ <a href="https://wbs.pekanbaru.go.id/storage/link-slotx-zgacor/" rel="dofollow">Slot Gacor</a>
+ <a href="https://stih-painan.ac.id/store/pg-slot/" rel="dofollow">SLOT DEMO</a>
 <a href="https://anjabpk.kemnaker.go.id/v3/uploads/demo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://divif2.kostrad.mil.id/app/pdemo/" rel="dofollow">SLOT DEMO</a>
 <a href="https://ptsp.halal.go.id/app/pgdemo/" rel="dofollow">SLOT DEMO</a>
