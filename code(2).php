7aa77253c6c993d9f1b06a214c3ed2a066c2163d
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-06-26 12:22:16 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -1,6 +1,6 @@
 <div style="display:none;">
 
-<a href="https://divif2.kostrad.mil.id/app/5lot-gacor/" rel="dofollow">SLOT GACOR</a>
+<a href="https://divif2.kostrad.mil.id/wp-gacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://kknreguler.unsam.ac.id/gacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://ptsp.halal.go.id/sl0t-gacor/" rel="dofollow">SLOT GACOR</a>
 <a href="https://jdihn.jenepontokab.go.id/images/sgacor/" rel="dofollow">SLOT GACOR</a>
@@ -9,28 +9,5 @@
 <a href="https://fekon.untad.ac.id/zgacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://sipp.upb.ac.id/-/zgacor/" rel="dofollow">slot gacor</a>
 <a href="https://fekon.untad.ac.id/" rel="dofollow">slot</a>
-<a href="https://eklipem-bbl.kemdikbud.go.id/upload/thai/" rel="dofollow">Slot Gacor</a>
-<a href="https://tracer.fekon.uniga.ac.id/smaxwin/" rel="dofollow">Slot Gacor</a>
-<a href="https://sijaki-dev.jombangkab.go.id/-/sgacor/" rel="dofollow">SLOT GACOR</a>
-<a href="https://jdihn.jenepontokab.go.id/images/serf/?cartel=hoki88" rel="dofollow">Slot Gacor</a>
-<a href="https://dashboard.fekon.uniga.ac.id/link-slotx-sgacor/" rel="dofollow">Slot Gacor</a>
-<a href="https://akademika.itbi.ac.id/assets/" rel="dofollow">SLOT GACOR</a>
-<a href="https://akuntansi.feb.upb.ac.id/depo/" rel="dofollow">Slot Gacor</a>
-<a href="https://sisfo.feb.upb.ac.id/js/" rel="dofollow">Slot Gacor</a>
-<a href="https://sisfo.feb.upb.ac.id/link-slotx-gacorz/" rel="dofollow">Slot Gacor</a>
-<a href="https://pmb.stiki.ac.id/demo/" rel="dofollow">Slot Demo</a>
-<a href="https://feb.upb.ac.id/link/" rel="dofollow">Slot Gacor</a>
-<a href="https://fekon.uniga.ac.id/-/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
-<a href="https://pmb.stiki.ac.id/system/" rel="dofollow">Slot Gacor</a>
-<a href="https://www.bpsdm.sultengprov.go.id/assets/slotx-zgacor/" rel="dofollow">Slot Gacor</a>
-<a href="https://ejurnal.iiq.ac.id/thailand/" rel="dofollow">Slot Gacor</a>
-<a href="https://sister.itbi.ac.id/css/" rel="dofollow">Slot Gacor</a>
-<a href="https://ujm.itbi.ac.id/pages/" rel="dofollow">Slot Gacor</a>
-<a href="https://alumni.uniflor.ac.id/raja168/" rel="dofollow">Slot Gacor</a>
-<a href="https://inlislite.solselkab.go.id/inlislite3/sgacor/" rel="dofollow">Slot Gacor</a>
-<a href="https://stih-painan.ac.id/store/pg-slot/" rel="dofollow">SLOT DEMO</a>
-<a href="https://anjabpk.kemnaker.go.id/v3/uploads/demo/" rel="dofollow">SLOT DEMO</a>
-<a href="https://divif2.kostrad.mil.id/app/pdemo/" rel="dofollow">SLOT DEMO</a>
-<a href="https://ptsp.halal.go.id/app/pgdemo/" rel="dofollow">SLOT DEMO</a>
-<a href="https://kknreguler.unsam.ac.id/demo/" rel="dofollow">SLOT DEMO</a>
-<a href="https://dashboard.global.unair.ac.id/repo/pdemo.html" rel="dofollow">SLOT DEMO</a>
+
+
