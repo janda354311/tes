400c282269197f13c5b357848f96925f77219753
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-07-05 09:50:06 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -8,6 +8,6 @@
 <a href="https://bem.fekon.uniga.ac.id/thai/" rel="dofollow">slot gacor</a>
 <a href="https://fekon.untad.ac.id/zgacor/" rel="dofollow">Slot Gacor</a>
 <a href="https://sipp.upb.ac.id/-/zgacor/" rel="dofollow">slot gacor</a>
-<a href="https://fekon.untad.ac.id/" rel="dofollow">slot</a>
+<a href="http://alumni.poltekpelsumbar.ac.id/demo/" rel="dofollow">slot demo</a>
 
 
