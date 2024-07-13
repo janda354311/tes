b9320becf39138e6170bde062abc1d90704d9203
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-07-13 08:28:43 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -1,13 +1,13 @@
 <div style="display:none;">
 
-<a href="https://elegalisirfkipunisri.com/vendor/" rel="dofollow">SLOT GACOR</a>
+
+<a href="https://pegawai.unkris.ac.id/app/link-xgacor/" rel="dofollow">slot gacor</a>
 <a href="https://pegawai.unkris.ac.id/menu/sgacor/" rel="dofollow">slot gacor</a>
 <a href="https://www.ptiunisri.com/sgacor-toto/" rel="dofollow">slot gacor</a>
 <a href="https://www.stiebipranaputra.ac.id/demo/" rel="dofollow">slot demo</a>
 <a href="https://pegawai.unkris.ac.id/assets/js/" rel="dofollow">slot gacor</a>
 <a href="https://bem.fekon.uniga.ac.id/thai/" rel="dofollow">slot gacor</a>
 <a href="https://wisuda.unkris.ac.id/admin/xgacor/" rel="dofollow">Slot Gacor</a>
-<a href="https://sipp.upb.ac.id/-/zgacor/" rel="dofollow">slot gacor</a>
 <a href="http://alumni.poltekpelsumbar.ac.id/demo/" rel="dofollow">slot demo</a>
 
 
