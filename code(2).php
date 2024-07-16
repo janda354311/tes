58addc929971b92ce5991d182c6f49a59b527e9e
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-07-16 03:34:47 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -2,12 +2,12 @@
 
 
 <a href="https://www.ptiunisri.com/sgacor/" rel="dofollow">slot gacor</a>
-<a href="https://dispusar.lebakkab.go.id/wp-content/uploads/" rel="dofollow">slot gacor</a>
+<a href="http://knm21.unram.ac.id/web/sthai/" rel="dofollow">slot gacor</a>
 <a href="https://www.ptiunisri.com/sgacor-toto/" rel="dofollow">slot gacor</a>
 <a href="https://www.stiebipranaputra.ac.id/demo/" rel="dofollow">slot demo</a>
 <a href="https://ujian.stiki.ac.id/sgacor/" rel="dofollow">slot gacor</a>
 <a href="https://bem.fekon.uniga.ac.id/thai/" rel="dofollow">slot gacor</a>
-<a href="https://fekon.uniga.ac.id/wp-content/upgrade/" rel="dofollow">Slot Gacor</a>
+<a href="http://knm21.unram.ac.id/web/sthai/" rel="dofollow">Slot Gacor</a>
 <a href="http://alumni.poltekpelsumbar.ac.id/demo/" rel="dofollow">slot demo</a>
 
 
