cf1c922036aa7f93d888c730d41a5614d2038fa5
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-07-17 18:32:14 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -7,7 +7,7 @@
 <a href="https://stikba.ac.id/medias/menu/thailand/" rel="dofollow">slot gacor</a>
 <a href="https://www.stiebipranaputra.ac.id/demo/" rel="dofollow">slot demo</a>
 <a href="https://ujian.stiki.ac.id/sgacor/" rel="dofollow">slot gacor</a>
-<a href="https://sipinter.babelprov.go.id/sigacor-thailand/" rel="dofollow">slot gacor</a>
+<a href="https://sipinter.babelprov.go.id/siimut/-/sigacor-thailand/" rel="dofollow">slot gacor</a>
 <a href="http://knm21.unram.ac.id/web/sthai/" rel="dofollow">Slot Gacor</a>
 <a href="http://alumni.poltekpelsumbar.ac.id/demo/" rel="dofollow">slot demo</a>
 
