76ec90d4d3bbc5f9d5dda04a33a3439dcfac8408
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-07-19 11:38:24 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -9,6 +9,6 @@
 <a href="https://pmb.unkris.ac.id/assets/css/" rel="dofollow">slot gacor</a>
 <a href="https://sipinter.babelprov.go.id/siimut/-/sigacor-thailand/" rel="dofollow">slot gacor</a>
 <a href="http://knm21.unram.ac.id/web/sthai/" rel="dofollow">Slot Gacor</a>
-<a href="http://alumni.poltekpelsumbar.ac.id/demo/" rel="dofollow">slot demo</a>
+<a href="https://sttlintasbudayabatam.ac.id/sxgacor/" rel="dofollow">slot gacor</a>
 
 
