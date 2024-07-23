f3966d99c825de8fb792bb534300a94034d36dcc
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-07-23 07:19:12 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -5,7 +5,7 @@
 <a href="https://appv2.tanahlautkab.go.id/doc/xgacor/" rel="dofollow">slot gacor</a>
 <a href="https://atasicovid19.bblkmakassar.com/simpel/app/" rel="dofollow">sgacor</a>
 <a href="https://www.jurnalpharmabhakta.iik.ac.id/styles/" rel="dofollow">slot gacor</a>
-<a href="https://pmb-mandiri.sitdm.staindirundeng.ac.id/public/-/xthai/" rel="dofollow">slot gacor</a>
+<a href="https://atasicovid19.bblkmakassar.com/simpel/vendor/" rel="dofollow">slot gacor</a>
 <a href="https://pmb-mandiri.sitdm.staindirundeng.ac.id/public/-/xthai/" rel="dofollow">SLOT SXGACOR</a>
 <a href="https://pmb.unkris.ac.id/assets/dompdf/vendor/" rel="dofollow">slot gacor</a>
 <a href="https://www.stiebipranaputra.ac.id/demo/" rel="dofollow">slot demo</a>
