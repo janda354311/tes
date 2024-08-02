32a08423881c5b51f4843dcaf0125ee464053467
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-08-01 23:06:15 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -8,7 +8,7 @@
 <a href="https://anjabpk.kemnaker.go.id/app/xmp/" rel="dofollow">Slot Gacor</a>
 <a href="https://kknreguler.unsam.ac.id/slobet/?gacor=bet+slot" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/slobet/?maxwin=slot+2024" rel="dofollow">Slot Gacor</a>
-<a href="https://atasicovid19.bblkmakassar.com/simpel/resources/" rel="dofollow">slot gacor</a>
+<a href="https://pipro.palukota.go.id/v1/js/" rel="dofollow">slot gacor</a>
 <a href="https://apps-rsunhas.com/simcen/app/" rel="dofollow">sgacor</a>
 <a href="https://indonesianresearchersforum.com/" rel="dofollow">slot 88</a>
 <a href="https://atasicovid19.bblkmakassar.com/simpel/vendor/" rel="dofollow">slot gacor</a>
