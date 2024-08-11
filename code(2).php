6cd3ab96ff15d0f587603e96606a4bcea035e4f8
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-08-10 23:12:55 -0400

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -1,10 +1,15 @@
 <div style="display:none;">
 
 <a href="https://hdpgsdi.com/guru/" rel="dofollow">Slot Gacor</a>
-<a href="https://sydemy.com/data/" rel="dofollow">Slot Gacor</a>
-<a href="https://daarulhuffazh.ponpes.id/app/" rel="dofollow">Slot Gacor</a>
 <a href="https://dreamjobsja.com/data/" rel="dofollow">Slot Gacor</a>
 <a href="https://dreamjobsja.com/file/" rel="dofollow">Slot Gacor</a>
+<a href="https://puprbadung.com/file/" rel="dofollow">Slot Gacor</a>
+<a href="https://puprbadung.com/data/" rel="dofollow">Slot Gacor</a>
+<a href="https://sydemy.com/data/" rel="dofollow">Slot Gacor</a>
+<a href="https://daarulhuffazh.ponpes.id/app/" rel="dofollow">Slot Gacor</a>
+<a href="https://iqra-publicschool.com/file/" rel="dofollow">Slot Gacor</a>
+<a href="https://iqra-publicschool.com/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://andamangrandholidays.com/data/" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/data/" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/file/" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/mantra/" rel="dofollow">Slot Gacor</a>
