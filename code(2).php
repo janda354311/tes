7f52e64063c84f57865f1df439a5bd129493e840
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-12-28 10:08:07 -0500

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -1,13 +1,12 @@
 <div style="display:none;">
 
-<a href="https://mobileicecube.com/" rel="dofollow">Slot Gacor</a>
+<a href="https://delanggu.klaten.go.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://wedi.klaten.go.id/" rel="dofollow">Slot Gacor</a>
 <a href="https://ngawen.klaten.go.id/" rel="dofollow">Slot Gacor</a>
-<a href="https://kodam14hasanuddin-tniad.mil.id" rel="dofollow">Slot Gacor</a>
 <a href="https://kel-kabupaten.klaten.go.id/" rel="dofollow">Slot Gacor</a>
 <a href="https://pembangunan.klaten.go.id" rel="dofollow">Slot Gacor</a>
 <a href="https://ejurnal.stih-painan.ac.id/" rel="dofollow">Slot Gacor</a>
 <a href="https://areioutdoorgear.com/" rel="dofollow">Slot Gacor</a>
-<a href="https://simelati.lampungtimurkab.go.id" rel="dofollow">Slot Gacor</a>
 <a href="https://stih-painan.ac.id/ini/" rel="dofollow">Slot Gacor</a>
 <a href="https://kknunsam.com/laman/" rel="dofollow">Slot Gacor</a>
 <a href="https://wattsdancemusic.com/pages/" rel="dofollow">Slot Gacor</a>
