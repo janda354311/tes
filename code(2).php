ba011089f11e860c8a9b4f81719914928e85854f
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2025-01-06 07:07:35 -0500

    Update code(2).php

--- code(2).php
+++ code(2).php
@@ -5,6 +5,7 @@
 <a href="https://inspektorat.klaten.go.id/" rel="dofollow">Slot Gacor</a>
 <a href="https://wattsdancemusic.com/" rel="dofollow">Slot Gacor</a>
 <a href="https://qqindobetslot88.com/" rel="dofollow">Slot Gacor</a>
+<a href="https://simpegbukp.jogjaprov.go.id/backend/assets/" rel="dofollow">Slot Gacor</a>
 <a href="http://alumni.poltekpelsumbar.ac.id/" rel="dofollow">Slot Gacor</a>
 <a href="https://www.pkbikabali.id/" rel="dofollow">Slot Gacor</a>
 <a href="https://pmb.itbm-bali.ac.id/" rel="dofollow">Slot Gacor</a>
